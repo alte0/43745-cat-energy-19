e123c723dfd4446ab25b0fb970e832b64a0c4ff3
Максим <ctpagateji@yandex.ru>
2020-04-17 18:20:49 +0500

    script

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,26 @@
 function ready() {
   var header = document.querySelector(".header");
   var toggle = document.querySelector(".header__toggle-menu");
+  var catalogLists = document.querySelector(".catalog__list");
+  var additionalList = document.querySelector(".additional-items__list");
 
-  function handerClick() {
+  function handerClickToggle() {
     header.classList.toggle("header_open");
   }
 
+  function handerClickOrder(evt) {
+    var target = evt.target;
+    console.log(target);
+    if (target.tagName === "A") {
+      console.log("preventDefault");
+      evt.preventDefault();
+    }
+  }
+
   header.classList.remove("header_open");
-  toggle.addEventListener("click", handerClick);
+  toggle.addEventListener("click", handerClickToggle);
+  catalogLists.addEventListener("click", handerClickOrder);
+  additionalList.addEventListener("click", handerClickOrder);
   svg4everybody();
 }
 
